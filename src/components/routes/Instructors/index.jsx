56170d5af7d728d6f855135d7432071514ef1b65
Dac099 <davidcejazapata@gmail.com>
2023-10-26 20:21:16 -0600

    Instructors route responsive complete

--- src/components/routes/Instructors/index.jsx
+++ src/components/routes/Instructors/index.jsx
@@ -11,6 +11,9 @@ import { LessonTable } from "../../LessonTable";
 import { auth } from "../../../firebase/firebase_sdk";
 import { onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { AiFillCaretDown } from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import marsbot from '/marsbot.png';
 
 function Instructors(){
   const navigate = useNavigate();
@@ -30,6 +33,7 @@ function Instructors(){
   const [ lessonData, setLessonData ] = React.useState(null);
   const [ modalMode, setModalMode ] = React.useState(null);
   let lessonsWithoutInstructor = [...lessons.filter(lessons => lessons.instructor === '')];
+  const [showLessons, setShowLessons] = React.useState(false);
 
 
   React.useEffect(() => {
@@ -64,89 +68,100 @@ function Instructors(){
 
       <section className={styles.container__instructors}>
 
-        <button 
-          type="button"
-          className={styles.add_instructor__btn}
-          onClick={() => {
-            setModalMode('instructors')
-            setShowModal(true);            
-          }}
-        >
-          Agregar maestro
-        </button>
+        <p className={styles.container__instructors_title}>Instructores</p>
 
-        <button 
-          type="button"
-          className={`${styles.add_instructor__btn} ${styles.no_intructor__btn}`}
-          onClick={() => {
-            setInstructorSelected('')
-          }}
-        >
-          Clases sin maestro
-        </button>
-
-        {instructors.map(instructor => (
-          <InstructorProfile 
-            key={instructor.phone}
-            data={instructor}
-            action={setInstructorSelected}
-            onEdit={true}
-            setInstructorData={setInstructorData}
-            setShowModal={setShowModal}
-            setModalMode={setModalMode}
-          />
-        ))}
-      </section>
-
-      <section className={
-          instructorSelected === ''
-          ? styles.container__lessons_no_intructor
-          : styles.container__lessons
-        }>
-
-        {instructorSelected !== '' &&
+        <section className={styles.container__instructors_cards}>
           <button 
             type="button"
-            className={styles.add_btn}
+            className={styles.add_instructor__btn}
             onClick={() => {
-              setModalMode('lessons')
-              setShowModal(true);
+              setModalMode('instructors')
+              setShowModal(true);            
             }}
           >
-            Aregar clase
-          </button>
-        }
+            <AiOutlineUserAdd />
+          </button>  
+          {instructors.map(instructor => (
+            <InstructorProfile 
+              key={instructor.phone}
+              data={instructor}
+              action={setInstructorSelected}
+              onEdit={true}
+              setInstructorData={setInstructorData}
+              setShowModal={setShowModal}
+              setModalMode={setModalMode}
+            />
+          ))}
+        </section>
 
-        {instructorSelected === '' &&
-          <>
-            <p className={styles.title}>Clases sin instructor asignado</p>
-            <article
-              className={styles.lessons}
-            >
-              {lessonsWithoutInstructor.map((lesson, index) => (
-                <LessonTable 
-                lesson={lesson}
-                key={index}
-                setLessonData={setLessonData}
-                setModalMode={setModalMode}
-                setShowModal={setShowModal}
+        {lessonsWithoutInstructor.length > 0 &&
+          <section className={styles.container__lessons_no_instructors}>
+
+            <div>
+              <p>Lecciones sin instructor</p>
+
+              <AiFillCaretDown 
+                onClick={() => setShowLessons(!showLessons)}
+                style={showLessons
+                  ? {transform: 'rotateZ(180deg)'}
+                  : {}
+                }
               />
-              ))}
-            </article>
-          </>          
-        }
-
-        {instructorSelected !== '' && instructorLessons.map((lesson, index) => (
-          <LessonTable 
-            lesson={lesson}
-            key={index}
-            setLessonData={setLessonData}
-            setModalMode={setModalMode}
-            setShowModal={setShowModal}
-          />
-        ))}
+            </div>
+            {showLessons &&
+              <div className={styles.lessons_without_instructors_container}>
+                {lessonsWithoutInstructor.map((lesson, index) => (
+                  <LessonTable 
+                    lesson={lesson}
+                    key={index}
+                    setLessonData={setLessonData}
+                    setModalMode={setModalMode}
+                    setShowModal={setShowModal}
+                  />                
+                ))}
+              </div>
+            }
+          </section>
+        }        
       </section>
-      
+
+      <section>
+        {instructorSelected === ''
+          ? 
+            <section>
+              <p className={styles.instructors_lessons__text}>Selecciona un maestro para poder ver sus clases</p>
+              <img 
+                src={marsbot} 
+                alt="Robot marsbot" 
+                className={styles.marsbot}
+              />
+            </section>
+          : 
+            <section className={styles.container__lessons}>
+              <button 
+                type="button"
+                className={styles.add_btn}
+                onClick={() => {
+                  setModalMode('lessons')
+                  setShowModal(true);
+                }}
+              >
+                Aregar clase
+              </button>
+              
+              {instructorLessons.map((lesson, index) => (
+                  <LessonTable 
+                    lesson={lesson}
+                    key={index}
+                    setLessonData={setLessonData}
+                    setModalMode={setModalMode}
+                    setShowModal={setShowModal}
+                  />
+              ))}
+            </section>
+        }
+      </section>
+
       {
         showModal && 
         modalMode === 'instructors' &&
